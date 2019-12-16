54f753874204109650702c52d57198a66c51b70c
carlosf96 <carlos.f.roque96@gmail.com>
2019-12-16 13:18:52 -0600

    Fixed typo in imprt to search styles

--- src/Pokemon/components/SearchType.jsx
+++ src/Pokemon/components/SearchType.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 
-import useStyles from '../../shared/atoms/styles';
+import useStyles from '../../shared/atoms/styles/SearchStyles';
 
 const SearchType = ({handleSearch}) => {
   const classes = useStyles();
