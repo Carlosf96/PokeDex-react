410f979941b547a02538aa6bf7fd861388e475ee
Carlosf96 <carlos.f.roque96@gmail.com>
2020-06-01 17:28:53 -0500

    Updates

---
+++ precache-manifest.d0fe3ae8090c61616c82b92da2b045ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7cff37772795c9c87df995ad504fb64",
+    "url": "/PokeDex-react/index.html"
+  },
+  {
+    "revision": "eddd4121c457d77b70b7",
+    "url": "/PokeDex-react/static/css/main.0d3a1458.chunk.css"
+  },
+  {
+    "revision": "5e7c3d6f54b3c0ae01d1",
+    "url": "/PokeDex-react/static/js/2.a971f0f4.chunk.js"
+  },
+  {
+    "revision": "0c78cc0d190eaeaa8133",
+    "url": "/PokeDex-react/static/js/3.894a8962.chunk.js"
+  },
+  {
+    "revision": "eddd4121c457d77b70b7",
+    "url": "/PokeDex-react/static/js/main.ea212efd.chunk.js"
+  },
+  {
+    "revision": "43738523ccd4bdcb212f",
+    "url": "/PokeDex-react/static/js/runtime-main.df4b86c3.js"
+  }
+]);
